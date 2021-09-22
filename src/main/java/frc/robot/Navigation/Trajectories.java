1f7ae0852df72e6883af0d12e739639ef559a623
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2021-09-22 12:34:12 -0400

    fix

--- src/main/java/frc/robot/Navigation/Trajectories.java
+++ src/main/java/frc/robot/Navigation/Trajectories.java
@@ -20,7 +20,7 @@ public class Trajectories {
     }
 
     static Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-            Arrays.asList(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(1.0, 0, new Rotation2d(0))), config);
+            Arrays.asList(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(10, 0, new Rotation2d(0))), config);
 
     static MecanumController mecanumController = new MecanumController(trajectory);
 
