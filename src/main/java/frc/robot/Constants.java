c8fb94861061043334b3298317d0117b8c50359a
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2021-11-15 12:19:37 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.util.Units;
 
 public class Constants {
     public static final class DriveConstants{
@@ -15,7 +14,7 @@ public class Constants {
         public static final int kBackLeftWheelPort = 3;
         public static final int kBackRightWheelPort = 5;
 
-        //drivetrain stuff TODO: CALIBRATE
+        //drivetrain stuff
         public static double speed = 1; //speed control 
 
         //PID stuff
@@ -23,15 +22,12 @@ public class Constants {
         public static double kI = 0;
         public static double kD = 0;
 
-        //random odometry stuff
-        public static double wheelRadius = 4;
-        public static double wheelRatio = 0.1; //encoder conversion factor
-        public static double gearRatio = 1;
-        public static double rpmToVelocity = 2 * Math.PI * Units.inchesToMeters(wheelRadius) / 60; //RPM of the wheel to speed of the wheel (in meters per second)
-
-        public static final double kTrackWidth = 0.5;
+        public static double ks = 1;
+        public static double kv = 0.8;
+        public static double ka = 0.15;
+        public static final double kTrackWidth = 0.53; //20.5 - 21 in
         // Distance between centers of right and left wheels on robot
-        public static final double kWheelBase = 0.7;
+        public static final double kWheelBase = 0.52; //20.25-20.5 in
         // Distance between centers of front and back wheels on robot
 
         public static final MecanumDriveKinematics kDriveKinematics =
@@ -41,17 +37,8 @@ public class Constants {
             new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
             new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-        
-        public static final int kEncoderCPR = 1024;
-
-        public static final double kWheelDiameterMeters = 0.15;
-
-        public static final double kEncoderDistancePerPulse =
-            // Assumes the encoders are directly mounted on the wheel shafts
-            (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
-        
         public static final SimpleMotorFeedforward kFeedforward =
-        new SimpleMotorFeedforward(1, 0.8, 0.15);
+        new SimpleMotorFeedforward(ks, kv, ka);
 
         // Example value only - as above, this must be tuned for your drive!
         public static final double kPFrontLeftVel = 0.5;
@@ -117,24 +104,4 @@ public class Constants {
             new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
       }
-      
-    //charachterisation constants
-    public static double ks = 0;
-    public static double kv = 0;
-    public static double ka = 0;
-    
-    //max velo and accel
-    public static double maxVelo = 0;
-    public static double maxAccel = 0;
-    
-    //joystick stuff TODO: NEEDS TO BE CALIBRATED
-    public static int port = 0; //joystick port
-    public static int LX = 0; //axis
-    public static int LY = 1;
-    public static int RX = 4;
-    public static int RY = 5;
-    public static int dpadUp = 5;
-    public static int dpadDown = 6;
-    public static int x = 3;
-
 }
