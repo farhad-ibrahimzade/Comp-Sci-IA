2520546d88765ca540bd5e58aa1670eed682d44b
Farhad Ibrahimzade <67722444+farhadnbps@users.noreply.github.com>
2021-12-09 08:54:10 -0500

    pid

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -2,8 +2,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 
-public class ShooterCommand extends CommandBase {
+public class ShooterCommand extends CommandBase extends PIDSubsystem {
     ShooterSubsystem m_shooter;
     private final double m_speed;
 
@@ -17,5 +20,18 @@ public class ShooterCommand extends CommandBase {
         m_shooter.shoot(m_speed);
         
     }
+    @Override
+  public void useOutput(double output, double setpoint) {
+    m_shooter.setVoltage(output + m_shooterFeedforward.calculate(setpoint));
+  }
+
+  @Override
+  public double getMeasurement() {
+    return m_shooter.getRate();
+  }
+
+  public boolean atSetpoint() {
+    return m_controller.atSetpoint();
+  }
     
 }
